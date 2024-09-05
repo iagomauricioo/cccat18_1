cd0607cfda2bd49f5079b2eac02590fd0ac76e1c
iago <iagomauricio7@gmail.com>
2024-09-05 11:15:53 -0300

    refatorando alguns IFs

--- src/signup.ts
+++ src/signup.ts
@@ -13,51 +13,40 @@ export const ERROR_MESSAGES = {
 	invalidCpf: "O CPF fornecido é inválido.",
 	invalidCarPlate: "A placa do carro fornecida é inválida.",
 };
-  
+
+const isValid = (nameIsValid: boolean, emailIsValid: boolean, cpfIsValid: boolean, carPlateIsValid: boolean) => {
+	return nameIsValid && emailIsValid && cpfIsValid && carPlateIsValid;
+}
 
 app.post("/signup", async function (req, res) {
 	const input = req.body;
 	const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
-	try {
-		const id = crypto.randomUUID();
-		let result;
-		const [acc] = await connection.query("select * from ccca.account where email = $1", [input.email]);
-		if (acc) result = ERROR_MESSAGES.emailExists;
-		if (!acc) {
-			const nameIsValid = input.name.match(/[a-zA-Z] [a-zA-Z]+/)
-			const emailIsValid = input.email.match(/^(.+)@(.+)$/)
-			const carPlateIsValid = input.carPlate.match(/[A-Z]{3}[0-9]{4}/);
-			const INSERT_INTO_ACCOUNTS = "insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, password) values ($1, $2, $3, $4, $5, $6, $7, $8)"
-			if (!nameIsValid) result = ERROR_MESSAGES.invalidName;
-			if (!emailIsValid) result = ERROR_MESSAGES.invalidEmail;
-			if (!validateCpf(input.cpf)) result = ERROR_MESSAGES.invalidCpf;
-			if (!carPlateIsValid) result = ERROR_MESSAGES.invalidCarPlate;
-			if (nameIsValid) {
-				if (emailIsValid) {
-					if (validateCpf(input.cpf)) {
-						if (carPlateIsValid) {
-							await connection.query(INSERT_INTO_ACCOUNTS, [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver, input.password]);
-							const obj = {
-								accountId: id
-							};
-							result = obj;
-						}
-					}
-				}
-
-			}
-
+	const id = crypto.randomUUID();
+	let result;
+	const [acc] = await connection.query("select * from ccca.account where email = $1", [input.email]);
+	if (acc) result = ERROR_MESSAGES.emailExists;
+	if (!acc) {
+		const nameIsValid = input.name.match(/[a-zA-Z] [a-zA-Z]+/)
+		const emailIsValid = input.email.match(/^(.+)@(.+)$/)
+		const carPlateIsValid = input.carPlate.match(/[A-Z]{3}[0-9]{4}/);
+		const cpfIsValid = validateCpf(input.cpf);
+		const INSERT_INTO_ACCOUNTS = "insert into ccca.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver, password) values ($1, $2, $3, $4, $5, $6, $7, $8)"
+		if (!nameIsValid) result = ERROR_MESSAGES.invalidName;
+		if (!emailIsValid) result = ERROR_MESSAGES.invalidEmail;
+		if (!validateCpf(input.cpf)) result = ERROR_MESSAGES.invalidCpf;
+		if (!carPlateIsValid) result = ERROR_MESSAGES.invalidCarPlate;
+		if (isValid(nameIsValid, emailIsValid, cpfIsValid, carPlateIsValid)) {
+			await connection.query(INSERT_INTO_ACCOUNTS, [id, input.name, input.email, input.cpf, input.carPlate, !!input.isPassenger, !!input.isDriver, input.password]);
+			const obj = {
+				accountId: id
+			};
+			result = obj;
 		}
-
-
-		if (typeof result === "object" && result.hasOwnProperty("accountId")) {
-			res.json(result);
-		  } else {
-			res.status(422).json({ message: result });
-		  }
-	} finally {
-		await connection.$pool.end();
 	}
+	const resultIsOk = (typeof result === "object" && result.hasOwnProperty("accountId"));
+	if (resultIsOk) res.json(result);
+	if (!resultIsOk) res.status(422).json({ message: result });
+	await connection.$pool.end();
 });
 
 app.listen(3000, () => {
--- test/signup.test.ts
+++ test/signup.test.ts
@@ -3,12 +3,9 @@ import app, { ERROR_MESSAGES } from '../src/signup';  // O caminho onde sua apli
 import pgp from "pg-promise";
 
 const connection = pgp()("postgres://postgres:123456@localhost:5432/app");
-
 beforeEach(async () => {
-  // Código para limpar ou resetar o banco de dados
   await connection.query("DELETE FROM ccca.account");
 });
-
 describe('POST /signup', () => {
   it('deve retornar 200 e criar uma conta', async () => {
     const response = await request(app)
@@ -16,18 +13,16 @@ describe('POST /signup', () => {
       .send({
         account_id: "id",
         name: "John Doe",
-        email: "johndoe@example.com", // Use um e-mail fixo, pois o DB será limpo antes
+        email: "johndoe@example.com",
         cpf: "132.991.114-85",
         carPlate: "ABC1234",
         isPassenger: true,
         isDriver: false,
         password: "mysecurepassword"
       });
-
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty('accountId');
   });
-
   it('Deve retonar 422 se o e-mail for inválido', async () => {
     const response = await request(app)
       .post('/signup')
@@ -40,11 +35,9 @@ describe('POST /signup', () => {
         isDriver: false,
         password: "mysecurepassword"
       });
-
     expect(response.status).toBe(422);
     expect(response.body.message).toBe(ERROR_MESSAGES.invalidEmail);
   });
-
   it('deve retornar 422 se nome do usuário tiver só o primeiro nome', async () => {
     const response = await request(app)
       .post('/signup')
@@ -61,7 +54,6 @@ describe('POST /signup', () => {
     expect(response.status).toBe(422);
     expect(response.body.message).toBe(ERROR_MESSAGES.invalidName);
   });
-
   it('Deve retonar 422 se o e-mail já existir', async () => {
     const response = await request(app)
       .post('/signup')
@@ -89,7 +81,6 @@ describe('POST /signup', () => {
       expect(response2.status).toBe(422);
       expect(response2.body.message).toBe(ERROR_MESSAGES.emailExists);
     });
-
   it('deve retornar 422 se a placa do carro for inválida', async () => {
     const response = await request(app)
       .post('/signup')
@@ -102,7 +93,6 @@ describe('POST /signup', () => {
         isDriver: false,
         password: "mysecurepassword"
       });
-
     expect(response.status).toBe(422);
     expect(response.body.message).toBe(ERROR_MESSAGES.invalidCarPlate);
   });
